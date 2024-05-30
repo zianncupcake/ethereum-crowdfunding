94ffbd92ffda36d912ba0a79c178d7273a2a7e48
zianncupcake <cheongziann@gmail.com>
2024-05-31 06:31:34 +0800

    start contract

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        address[] donators;
+        uint[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline) public returns (uint256)
+
+}
